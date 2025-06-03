173a415f3c122a5e1805ee77345b39abc4d4fb1c
webahna <alexrmzqzda@gmail.com>
2025-06-03 16:58:29 -0700

    add site in astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://decostoreinstalaciones.com",
   vite: {
     plugins: [tailwindcss()],
   },
